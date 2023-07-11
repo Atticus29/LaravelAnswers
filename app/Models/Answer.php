634daba97933bf422320dc130fc5e8a52b4a746d
Mark <mark.aaron.fisher@gmail.com>
2023-07-10 17:02:15 -0700

    create answer model and connect the answer and question models to each other

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Question extends Model
+class Answer extends Model
 {
     use HasFactory;
 
-    public function answers(){
-        return $this->hasMany('App\Answer');
+    public function question(){
+        return $this->belongsTo('App\Question');
     }
 }
